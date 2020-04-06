485504ce079b8a5c1712f4d2f3ac0420923232d7
mannavapavan <63238382+mannavapavan@users.noreply.github.com>
2020-04-06 23:52:07 +0100

    Update terraform-destroy.sh

--- jenkins-packer-demo/scripts/terraform-destroy.sh
+++ jenkins-packer-demo/scripts/terraform-destroy.sh
@@ -3,7 +3,7 @@ set -ex
 
 AWS_REGION="eu-west-1"
 cd jenkins-packer-demo
-S3_BUCKET=`aws s3 ls --region $AWS_REGION |grep terraform-state |tail -n1 |cut -d ' ' -f3`
+S3_BUCKET=`aws s3 ls --region $AWS_REGION |grep terraform-state |sort|tail -n1 |cut -d ' ' -f3`
 sed -i 's/terraform-state-xx70dpnh/'${S3_BUCKET}'/' backend.tf
 sed -i 's/#//g' backend.tf
 terraform destroy
