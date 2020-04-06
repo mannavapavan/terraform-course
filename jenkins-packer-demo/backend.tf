a42f96d3d257b175063c725769322d5c4d40731f
mannavapavan <63238382+mannavapavan@users.noreply.github.com>
2020-04-06 14:32:28 +0100

    Update backend.tf

--- jenkins-packer-demo/backend.tf
+++ jenkins-packer-demo/backend.tf
@@ -1,6 +1,6 @@
 #terraform {
 #  backend "s3" {
-#    bucket = "terraform-state-xx70dpnh"
+#    bucket = "terraform-state-stbnemxi"
 #    key    = "terraform.tfstate"
 #    region = "eu-west-1"
 #  }
