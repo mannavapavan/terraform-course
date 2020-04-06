f963065d679a7a1ec5fca42e9803c21eba0f6f80
mannavapavan <63238382+mannavapavan@users.noreply.github.com>
2020-04-07 00:12:37 +0100

    Update jenkins-run-terraform.sh

--- jenkins-packer-demo/scripts/jenkins-run-terraform.sh
+++ jenkins-packer-demo/scripts/jenkins-run-terraform.sh
@@ -3,8 +3,11 @@ set -ex
 
 AWS_REGION="eu-west-1"
 cd jenkins-packer-demo
+ls -altr
+cat backend.tf
 S3_BUCKET=`aws s3 ls --region $AWS_REGION |grep terraform-state |sort|tail -n1 |cut -d ' ' -f3`
 sed -i 's/terraform-state-xx70dpnh/'${S3_BUCKET}'/' backend.tf
 sed -i 's/#//g' backend.tf
+cat backend.tf
 terraform init
 terraform destroy -auto-approve
